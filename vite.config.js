4505a70d476a210ed2bc318b0d42bdbf01a471f5
Leonardo <terlizzileonardo@gmail.com>
2025-01-21 11:19:47 -0300

    added more @assets configs in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,24 @@ export default defineConfig({
   resolve: {
     alias: {
       "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@assets/craze": fileURLToPath(
+        new URL("./src/assets/craze", import.meta.url)
+      ),
+      "@assets/DevCards": fileURLToPath(
+        new URL("./src/assets/DevCards", import.meta.url)
+      ),
+      "@assets/Eshop": fileURLToPath(
+        new URL("./src/assets/Eshop", import.meta.url)
+      ),
+      "@assets/pakapaka": fileURLToPath(
+        new URL("./src/assets/pakapaka", import.meta.url)
+      ),
+      "@assets/vcm": fileURLToPath(
+        new URL("./src/assets/vcm", import.meta.url)
+      ),
+      "@assets/videos": fileURLToPath(
+        new URL("./src/assets/videos", import.meta.url)
+      ),
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
